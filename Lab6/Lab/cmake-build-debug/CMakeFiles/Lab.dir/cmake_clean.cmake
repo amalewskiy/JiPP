f12abf36c9bbea8277eebbfb50e12ed9b314334b
Pavel Milevskiy <amalewskiy@gmail.com>
2020-04-02 16:19:48 +0200

    First commit

---
+++ Lab6/Lab/cmake-build-debug/CMakeFiles/Lab.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab.dir/main.c.o"
+  "Lab"
+  "Lab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
