00f23781dfbedab4246382a93c2f068ecd586800
Pavel Milevskiy <amalewskiy@gmail.com>
2020-04-03 20:01:10 +0200

    stable11

--- Lab6/Lab/main.c
+++ Lab6/Lab/main.c
@@ -7,38 +7,40 @@
 
 FILE *fd;
 
-void ile_slow(char *te, int *LDL, int *LML, int *LPZ) {
+void zad11(char *te, int *LDL, int *LML, int *LPZ) {
     char p = ' ';
-    while (p = *te++)
-        if ((int) p > 64 && (int) p < 91) *LDL++;
-        else if ((int) p > 96 && (int) p < 123) *LML++;
-        else *LPZ++;
+    while (p = *te++) {
+        if ((int) p > 64 && (int) p < 91) {
+            *LDL = *LDL + 1;
+        } else if ((int) p > 96 && (int) p < 123) {
+            *LML = *LML + 1;
+        } else *LPZ = *LPZ + 1;
+    }
 }
 
 int main() {
-    char *d[MAX_LINES], bufor[MAX_LINE];
-    int len, i = 0, LL = 0, *LDL = 0, *LML = 0, *LPZ = 0;
     if (!(fd = fopen("DANE.txt", "r"))) {
-        printf("Blad otwarcia zbioru\n");
+        printf("EROR WITH FILE\n");
         exit(2);
     }
-
-    while (i < MAX_LINES && fgets(bufor, MAX_LINE, fd)) {
+    char *d[MAX_LINES], bufor[MAX_LINE];
+    int len = 0, LL = 0, LDL = 0, LML = 0, LPZ = 0;
+    while (LL < MAX_LINES && fgets(bufor, MAX_LINE, fd)) {
         len = strlen(bufor);
         bufor[len - 1] = '\0';
-        if ((d[i] = (char *) malloc((unsigned) len)) == (char *) NULL) {
+        if ((d[LL] = (char *) malloc((unsigned) len)) == (char *) NULL) {
             printf("Brak pamieci\n");
             exit(3);
         }
-        strcpy(d[i], bufor);
-        ile_slow(d[i], LDL, LML, LPZ);
+        strcpy(d[LL], bufor);
+        zad11(d[LL], &LDL, &LML, &LPZ);
         LL++;
-        i++;
     }
     fclose(fd);
     fd = NULL;
-    printf("Liczba duzych liter: %d\n", *LDL);
-    printf("Liczba malych liter: %d\n", *LML);
-    printf("Liczba dpozostalych znakow: %d\n", *LPZ);
-    system("pause");
+    printf("Liczba linij: %d\n", LL);
+    printf("Liczba duzych liter: %d\n", LDL);
+    printf("Liczba malych liter: %d\n", LML);
+    printf("Liczba pozostalych znakow: %d\n", LPZ);
+    return 0;
 }
