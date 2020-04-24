170dd086715fc80f063614bee048e2b3381f8b39
Pavel Milevskiy <amalewskiy@gmail.com>
2020-04-24 17:33:50 +0200

    stable

--- Lab8/util_2.c
+++ Lab8/util_2.c
@@ -1,4 +1,19 @@
-//
-// Created by lewskiy_ on 24.04.2020.
-//
+#include<stdio.h>
+#include<stdlib.h>
 
+double *DajWekt(int n) {
+    double *xy = (double *) malloc(n);
+    return xy;
+}
+
+void CzytWekt(FILE *fd, double *we, int n) {
+    for (int k = 0; k < n; k++)
+        fscanf(fd, "%lf", &we[k]);
+}
+
+void PiszWekt(FILE *fw, double *we, int n) {
+    for (int k = 0; k < n; k++) {
+        fprintf(fw, "%lf ", we[k]);
+        if (!((k + 1) % 5)) fprintf(fw, "\n");
+    }
+}
